3f90ad10dc158df31a8e3f579e57da050931fae3
Richard de Weerd <richard@deweerd.biz>
2019-05-24 14:10:27 +0200

    Mail thread fix

--- app/email.py
+++ app/email.py
@@ -13,7 +13,9 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
+    with current_app.app_context():
+        mail.send(msg)
+    #Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
 
 
         
