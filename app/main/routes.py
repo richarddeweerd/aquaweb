297eee79f0512bee26f8b1e8057f05f14d1e51ad
Richard de Weerd <richard@deweerd.biz>
2019-05-26 22:32:14 +0200

    Temperature read test

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 '''Routes for main BP'''
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, jsonify
 from flask_login import current_user, login_required
 from app import DB as db
 #from flask import render_template, flash, redirect, url_for, request
@@ -28,3 +28,24 @@ def index():
 def secure():
     '''secure page'''
     return render_template('index.html')
+
+@bp.route('/data/current')
+#@login_required
+def current_data():
+    '''json data page'''
+    temp = -100
+    try:
+        fileh = open("/sys/bus/w1/devices/28-0415a27b39ff/w1_slave", 'r')
+        # Store configuration file values
+        lines = fileh.readlines()
+        fileh.close()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp = lines[1][equals_pos + 2:]
+            temp = float(temp)/1000
+    except FileNotFoundError:
+        # Keep preset values
+        pass
+
+    data = {'T1': temp}
+    return jsonify(data)
