0691d55fe964a69ff9e31e584fc30a975e58e524
Richard de Weerd <richard@deweerd.biz>
2019-07-01 23:15:34 +0200

    Added temperature form

--- app/settings/forms.py
+++ app/settings/forms.py
@@ -4,3 +4,12 @@ from wtforms import StringField, SubmitField, SelectField,PasswordField
 #from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 #from wtforms.validators import EqualTo
+
+class TemperatureForm(FlaskForm):
+    '''Form to edit the Temperature sensors'''
+    t0 = SelectField('t0', coerce=str)
+    t1 = SelectField('t1', coerce=str)
+    t2 = SelectField('t2', coerce=str)
+    t3 = SelectField('t3', coerce=str)
+    t4 = SelectField('t4', coerce=str)
+    submit = SubmitField('Submit')
--- app/settings/routes.py
+++ app/settings/routes.py
@@ -2,7 +2,7 @@
 from flask import render_template
 from flask_login import login_required
 from app.settings import BP as bp
-
+from app.settings.forms import TemperatureForm
 #from flask import render_template, redirect, flash, request, url_for
 #from werkzeug.urls import url_parse
 #from flask_login import login_user, logout_user, current_user
@@ -27,5 +27,13 @@ def outputs():
 @bp.route('/settings/temperature')
 @login_required
 def temperature():
-    '''Screen with measurment types list'''
-    return render_template('settings/temperature.html')
+    '''Screen to link temperature sensors'''
+    form = TemperatureForm()
+    #form.level.choices = [(l.id, l.levelname) for l in UserLevel.query.order_by('id')]
+    senlist = []
+    senlist.append("Sensor 1")
+    senlist.append("Sensor 2")
+    form.t0.choices = senlist
+
+
+    return render_template('settings/temperature.html', title='Temperature sensor setup', form=form)
