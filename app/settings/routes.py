70e0aca21edb056c28e2444ccee30e17f12d9e30
Richard de Weerd <richard@deweerd.biz>
2019-07-05 23:02:12 +0200

    Started with save routines

--- app/settings/routes.py
+++ app/settings/routes.py
@@ -1,5 +1,6 @@
 '''Settings module routes'''
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
+
 from flask_login import login_required
 from app.settings import BP as bp
 from app.settings.forms import TemperatureForm
@@ -31,11 +32,9 @@ def outputs():
 def temperature():
     '''Screen to link temperature sensors'''
     cfgdata = ReefConfig.query.filter_by(id=1).first_or_404()
-
     rawdata = file_to_dict("/var/aquarium/rawtemp")
-
     form = TemperatureForm()
-    #form.level.choices = [(l.id, l.levelname) for l in UserLevel.query.order_by('id')]
+
     senlist = []
     sensoritem = ("none", "None")
     senlist.append(sensoritem)
@@ -43,10 +42,25 @@ def temperature():
         sensoritem = (sens["id"], sens["id"])
         senlist.append(sensoritem)
 
-    form.t0.choices = senlist
-    form.t1.choices = senlist
-    form.t2.choices = senlist
-    form.t3.choices = senlist
-    form.t4.choices = senlist
-    form.t1_name.data = cfgdata.t1_name
+
+    if form.validate_on_submit():
+        #_account.username = form.username.data
+        #_account.full_name = form.full_name.data
+        #_account.email = form.email.data
+        #if _account.id != current_user.id:
+        #    _account.level_id = form.level.data
+        #db.session.commit()
+        flash('Your changes have been saved.')
+        #return render_template('settings/temperature.html', title='Temperature sensor setup', form=form, sensors=rawdata)
+
+    elif request.method == 'GET':
+        form.t0.choices = senlist
+        form.t1.choices = senlist
+        form.t2.choices = senlist
+        form.t3.choices = senlist
+        form.t4.choices = senlist
+        form.t1_name.data = cfgdata.t1_name
+        form.t2_name.data = cfgdata.t2_name
+        form.t3_name.data = cfgdata.t3_name
+        form.t4_name.data = cfgdata.t4_name
     return render_template('settings/temperature.html', title='Temperature sensor setup', form=form, sensors=rawdata)
