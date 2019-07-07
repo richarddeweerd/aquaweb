d28938cdbe3928b5f21fa1767541d5fb11b8fb06
Richard de Weerd <richard@deweerd.biz>
2019-07-07 12:01:04 +0200

    Added config saving

--- app/settings/routes.py
+++ app/settings/routes.py
@@ -3,6 +3,8 @@ from flask import render_template, redirect, url_for, flash, request
 
 from flask_login import login_required
 from app.settings import BP as bp
+from app import DB as db
+
 from app.settings.forms import TemperatureForm
 #from flask import render_template, redirect, flash, request, url_for
 #from werkzeug.urls import url_parse
@@ -49,12 +51,17 @@ def temperature():
     form.t4.choices = senlist
     
     if form.validate_on_submit():
-        #_account.username = form.username.data
+        cfgdata.username = form.username.data
+        cfgdata.t1_name = form.t1_name.data
+        cfgdata.t2_name = form.t2_name.data
+        cfgdata.t3_name = form.t3_name.data
+        cfgdata.t4_name = form.t4_name.data
+
         #_account.full_name = form.full_name.data
         #_account.email = form.email.data
         #if _account.id != current_user.id:
         #    _account.level_id = form.level.data
-        #db.session.commit()
+        db.session.commit()
         flash('Your changes have been saved.')
         #return render_template('settings/temperature.html', title='Temperature sensor setup', form=form, sensors=rawdata)
 
